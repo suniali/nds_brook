ce85a6bb6f41c092d4f62c9d3777ca9c0317075f
jarvis <jarvismatroki@gmail.com>
2022-10-19 17:53:29 +0330

    edit signup

--- signup_brook.py
+++ signup_brook.py
@@ -1,5 +1,8 @@
 import requests
 import json
+import colorama as cr
+
+cr.init(autoreset=True)
 
 signup_url='https://irvpn.co/userapi/signup'
 signin_url='https://irvpn.co/userapi/signin'
@@ -26,17 +29,32 @@ if str(res)=="<Response [200]>" :
         prime_link=res3.text.replace("198.244.140.109","195.28.11.20")
         print("Connection Link Is:")
         print(prime_link)
+
+        # Opening JSON file
+        with open('transfers_brook_list.json', 'r') as openfile:
+            # Reading from json file
+            json_objects = json.load(openfile)
+            openfile.close()
+
+        json_objects.append({ "user": username, "transfer": 0 })
+        json_objects=json.dumps(json_objects)
+        # Writing file
+        with open("transfers_brook_list.json", "w") as outfile:
+            outfile.write(json_objects)
+            outfile.close()
+
         # Writing Data To File
         s=f"username: {username} password: {password} link: {prime_link}"
         with open("nds_brook_username_password_list.txt", "a") as myfile:
             myfile.write(s + "\n")
             myfile.close()
+
+        print(f"{cr.Fore.GREEN}All Data Writed Successfully.")
     else:
-        print("Wrong!")
-        print(res)
-        print(res.text)
+        print(f"{cr.Fore.RED}Wrong! \n {cr.Fore.RED}{res} \n {cr.Fore.RED}{res.text}")
 
 else :
-    print("The SignUp Was Failed!")
+    print(f"{cr.Fore.RED}The SignUp Was Failed!")
     print(res)
+    print(res.text)
 
