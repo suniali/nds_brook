10403ca5d94dee4aa18a32ef65f3c51dd4abdb1a
jarvis <jarvismatroki@gmail.com>
2022-10-29 15:43:32 +0330

    update set transfer brook

--- set_transfer_brook.py
+++ set_transfer_brook.py
@@ -7,38 +7,87 @@ from mysql.connector import Error
 
 cr.init(autoreset=True)
 
+class SetData:
+    def __init__(self) -> None:
+        pass
+
+    def get_users(self):
+        counter = 0
+        while counter <= 5:
+            try:
+                connection = mysql.connector.connect(host='198.244.140.109',
+                                                     database='brook',
+                                                     user='jarvis',
+                                                     password='MH75d$4s#d9.f6bF')
+                if connection.is_connected():
+                    cursor = connection.cursor()
+                    query = "SELECT * FROM user"
+                    cursor.execute(query)
+                    res = cursor.fetchall()
+                    return res
+                else:
+                    return False
+
+            except Error as e:
+                print(
+                    f"{cr.Fore.RED}Error while connecting to Database For Getting Users", e)
+                if connection.is_connected():
+                    cursor.close()
+                    connection.close()
+                return False
+
+    def write_transfers_users_data(self,users_data:list) -> None:
+        d=[]
+        #Writing To Transfer Brook List Database
+        for user in users_data:
+            s={"id":user[0],"user": user[1],"transfer": user[7]}
+            d.append(s)
+
+        # print(d)
+        json_object = json.dumps(d)
+        with open("transfers_brook_list.json", "w") as outfile:
+            outfile.write(json_object)
+
+        print(f"{cr.Fore.GREEN}Data Downloaded Successfuly.")     
+
+    def read_permanent_baned_users(self):
+        with open('permanent_ban_users.txt', 'r') as openfile:
+            # Reading from json file
+            baned_users_list = openfile.readlines()
+            openfile.close()
+
+        return baned_users_list
+
+    def reset_baned_users_file(self)->None:
+        # Read Permanent Baned Users File
+        permanent_baned_users=self.read_permanent_baned_users()
+
+        # Rest Baned Users List Text File
+        with open('baned_users_list.txt', 'w') as openfile:
+            # Writing from List
+            for user in permanent_baned_users:
+                temp_user_info = user.split(',')
+                u = f"{temp_user_info[0]},{temp_user_info[1]}"
+                openfile.write(u)
+
+            openfile.close()
+
+
+
 def start():
-    try:
-        connection = mysql.connector.connect(host='198.244.140.109',
-                                             database='brook',
-                                             user='jarvis',
-                                             password='MH75d$4s#d9.f6bF')
-        if connection.is_connected():
-            cursor = connection.cursor()
-            query="SELECT * FROM user"
-            cursor.execute(query)
-            res=cursor.fetchall()
+    sd = SetData()
+    get_users_result = sd.get_users()
 
-            d=[]
-            #Writing to database
-            for row in res:
-                s={"id":row[0],"user": row[1],"transfer": row[7]}
-                d.append(s)
+    if get_users_result is False:
+        print(f"{cr.Fore.RED}Users Info Was Not Downloaded!!!")
+        exit()
 
-            # print(d)
-            json_object = json.dumps(d)
-            # print(json_object)
-            with open("transfers_brook_list.json", "w") as outfile:
-                outfile.write(json_object)
+    else:
+        sd.write_transfers_users_data(get_users_result)
+        sd.reset_baned_users_file()
+                
 
-            print(f"{cr.Fore.GREEN}Data Downloaded Successfuly.")
-
-    except Error as e:
-        print(f"{cr.Fore.RED}Error while connecting to MySQL", e)
-    finally:
-        if connection.is_connected():
-            cursor.close()
-            connection.close()
+    
 
 # -------------------------------------------------------------------------------------------
 schedule.every(24).hour.do(start)
